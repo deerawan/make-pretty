300373a7dd181d133bfb1e18b6b6659c93f6b688
Budi Irawan <deerawan@gmail.com>
2018-06-20 11:25:17 +0700

    use inquirer test

--- __test__/cli.test.js
+++ __test__/cli.test.js
@@ -1,31 +1,56 @@
-const copy = require('copy');
 const childProcess = require('child_process');
 const {promisify} = require("es6-promisify");
 const editJsonFile = require('edit-json-file');
+const cpx = require('cpx');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const inquirerTest = require('inquirer-test');
 
-const copyOne = promisify(copy.one);
+const copy = promisify(cpx.copy);
+const timeout = 15000;
+const currentDir = __dirname;  
+const cliPath = `${currentDir}/cli.js`;
 let file;
 
-beforeAll(done => {
-  const currentDir = __dirname;  
-  const copyOptions = { flatten: true };  
-  Promise.all([
-    copyOne(`${currentDir}/fixtures/package.json`, currentDir, copyOptions),
-    copyOne(`${currentDir}/../src/cli.js`, currentDir, copyOptions)
-  ])
-  .then(() => { 
-    childProcess.execSync('node cli.js', { cwd: currentDir, stdio: [0, 1, 2]});
+beforeAll(async () => {  
+  const copyOptions = { };  
+  childProcess.execSync('git clean -x -f', { cwd: currentDir, stdio: [0, 1, 2]});  
+  await Promise.all([
+    copy(`${currentDir}/fixtures/package.json`, currentDir, copyOptions),
+    copy(`${currentDir}/../src/cli.js`, currentDir, copyOptions),    
+    copy(`${currentDir}/../src/templates/.*`, `${currentDir}/templates`, copyOptions),    
+  ]);    
+}, timeout);
+
+describe('Javascript language is chosen', function() {
+  let result;
+  let file;
+
+  beforeAll(async () => {
+    result = await inquirerTest([cliPath], [inquirerTest.ENTER]);    
+    console.log(result);
     file = editJsonFile(`${currentDir}/package.json`);
-    done()
-  });
-});
+  }, timeout);
 
-test('has lint-staged to run in precommit', () => {  
-  expect(file.get("scripts.precommit")).toEqual('lint-staged');
-});
+  test('javascript is chosen', () => {
+    expect(result).toMatch('javascript');
+  })
 
-test('has lint-staged configuration', () => {
-  expect(file.get("lint-staged")).toEqual({
-    "*.{js,json,css,md}": ["prettier --write", "git add"]
+  test('has lint-staged to run in precommit', () => {  
+    expect(file.get("scripts.precommit")).toEqual('lint-staged');
   });
-})
+
+  test('has lint-staged configuration', () => {
+    expect(file.get("lint-staged")).toEqual({
+      "*.{js,json,css,md}": ["prettier --write", "git add"]
+    });
+  });
+
+  test('has prettier configuration files', () => {
+    const prettierIgnoreStats = fs.statSync(`${currentDir}/.prettierignore`);
+    const prettierRcStats = fs.statSync(`${currentDir}/.prettierrc`);
+    
+    expect(prettierIgnoreStats.isFile()).toEqual(true);
+    expect(prettierRcStats.isFile()).toEqual(true);
+  })
+});
--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,25 @@
-const copy = require('copy');
 const inquirer = require('inquirer');
 const npmInstallPackage = require('npm-install-package');
 const editJsonFile = require('edit-json-file');
 const {promisify} = require("es6-promisify");
+const cpx = require('cpx');
+const chalk = require('chalk');
+const fs = require('fs');
+const warning = chalk.keyword('orange');
 
-const copyEach = promisify(copy.each);
+const copy = promisify(cpx.copy);
 const templateDir = `${__dirname}/templates`;
+console.log('templates directory', templateDir);
 
-const packageJsonPath = `${__dirname}/package.json`;
+const currentDir = __dirname;
+const packageJsonPath = `${currentDir}/package.json`;
 console.log('package json found', packageJsonPath);
 let packageFile = editJsonFile(packageJsonPath);
 
-const devDeps = [
+const requiredDevDeps = [
   'lint-staged',
-  'husky'
+  'husky',
+  'prettier'
 ];
 
 const questions = [
@@ -22,7 +28,7 @@ const questions = [
     name: 'language',
     message: 'What language is it?',
     choices: ['Javascript', 'Typescript'],
-    filter: function(val) {
+    filter(val) {
       return val.toLowerCase();
     }
   }
@@ -31,15 +37,19 @@ const questions = [
 inquirer.prompt(questions).then(answers => {  
   console.log(JSON.stringify(answers, null, '  '));
 
+  if (answers.language === 'typescript') {
+
+  }
+
   console.log('install dev dependencies');
-  npmInstallPackage(devDeps, {
-    saveDev: true,    
-  }, function(err) {
-    if (err) {
+  return installDevPackages(requiredDevDeps)
+    .then(() => Promise.all([      
+      copyPrettierTemplates(),
+      modifyPackageFile()
+    ]))
+    .catch(err => {
       throw err;
-    }
-    modifyPackageFile();
-  });  
+    })
 });
 
 function modifyPackageFile() {
@@ -55,13 +65,58 @@ function modifyPackageFile() {
   packageFile.set('scripts.precommit', jsonTemplate['scripts']['precommit']);
   packageFile.set('lint-staged', jsonTemplate['lint-staged']);
 
-  packageFile.save();
+  return new Promise((resolve, reject) => {
+    packageFile.save(err => {
+      if (err) {
+        reject(err);
+      }
+      
+      resolve();
+    });  
+  });
 }
 
 function copyPrettierTemplates() {
-  
-  copyEach([
-    `${templateDir}/.prettierignore`,
-    `${templateDir}/.prettierrc`
-  ])
+  console.log('copying prettier files');
+  return copy(`${templateDir}/.*`, currentDir)
+  .then(() => {
+    console.log('prettier files are copied');
+    return Promise.resolve();
+  })
+  .catch(err => {
+    throw err;
+  })
+}
+
+function configurePrettierForTypescript() {
+  const tslintFilePath = `${currentDir}/.tslint.json`;
+  const tslintStats = fs.statSync(tslintFilePath);
+  if (tslintStats.isFile()) {
+    console.log(warning('tslint.json is exist, adding tslint-config-prettier to `extends`...'));      
+    return installDevPackages(['tslint-config-prettier'])
+      .then(() => {
+        const tslintFile = editJsonFile(tslintFilePath);
+        const currentExtends = tslintFile.get('extends');
+        const newExtends = [...currentExtends, 'tslint-config-prettier'];
+        tslintFile.set('extends', newExtends);  
+
+        return Promise.resolve();
+      })
+  }
+
+  return Promise.resolve();
+}
+
+function installDevPackages(packages) {
+  return new Promise((resolve, reject) => {    
+    npmInstallPackage(packages, {
+      saveDev: true,    
+    }, function(err) {
+      if (err) {
+        console.log('woi error');
+        reject(err);
+      }
+      resolve();
+    });  
+  });
 }
